46cea5474d95c3a4f3f4807559a6c3a8996638b1
Jack Gong <jackgong48@gmail.com>
2024-04-02 15:47:14 +1300

    FIXED: images

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,27 +1,28 @@
 import Marquee from "react-fast-marquee";
-import useImage from "src/utils/useImage";
+import lottoImg from "@assets/images/sponsors/lotto.png";
+import ministryImg from "@assets/images/sponsors/ministry.png";
+import skycityImg from "@assets/images/sponsors/skycity.png";
+import toddImg from "@assets/images/sponsors/todd.png";
+
+const images = [lottoImg, ministryImg, skycityImg, toddImg]
 
 const Sponsors = () => {
-    const sponsorImages = [
-        "lotto.png", "ministry.png", "skycity.png", "todd.png"
-    ]
 
     return (
         <div className="flex flex-col">
             <Marquee autoFill={true}>
                 {
-                    sponsorImages.map(img => {
-                        const { loading, error, image } = useImage(`../assets/images/sponsors/${img}`);
-                        if (image) {
-                            return (
-                                <img
-                                    key={img}
-                                    src={`${(image as any).src}`}
-                                    alt="sponsor"
-                                    className="w-52 max-md:w-20"
-                                />
-                            );
-                        }
+                    images.map(img => {
+
+                        return (
+                            <img
+                                key={img.src}
+                                src={`${img.src}`}
+                                alt="sponsor"
+                                className="w-52 max-md:w-20"
+                            />
+                        );
+
                     })
                 }
             </Marquee>
