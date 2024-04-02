6d18d77a8d0ed909f1a3e5526a79687efed79100
Jack Gong <jackgong48@gmail.com>
2024-04-02 15:40:22 +1300

    ADDED: marquee

---
+++ src/components/Sponsors.tsx
@@ -0,0 +1,34 @@
+import Marquee from "react-fast-marquee";
+import useImage from "src/utils/useImage";
+
+const Sponsors = () => {
+    const sponsorImages = [
+        "lotto.png", "ministry.png", "skycity.png", "todd.png"
+    ]
+
+    return (
+        <div className="flex flex-col">
+            <Marquee autoFill={true}>
+                {
+                    sponsorImages.map(img => {
+                        const { loading, error, image } = useImage(`../assets/images/sponsors/${img}`)
+                        if (image) {
+                            return (
+                                <img
+                                    key={img}
+                                    src={`${image.src}`}
+                                    alt="sponsor"
+                                    className="w-52 max-md:w-20"
+                                />
+                            )
+                        }
+
+
+                    })
+                }
+            </Marquee>
+        </div>
+    );
+};
+
+export default Sponsors;
---
+++ src/utils/useImage.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+
+const useImage = (filePath: string) => {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const [image, setImage] = useState(null)
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            try {
+                console.log(filePath)
+                const response = await import(filePath) // change relative path to suit your needs
+                setImage(response.default)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchImage()
+    }, [filePath])
+
+    return {
+        loading,
+        error,
+        image,
+    }
+}
+
+export default useImage
